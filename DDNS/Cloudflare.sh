be7f554b11e4fd54cbe9d73cbf8cc9cc2d931529
Amaury <amaury.data@gmail.com>
2022-04-23 21:52:44 +0800

    Update Cloudflare.sh

--- DDNS/Cloudflare.sh
+++ DDNS/Cloudflare.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#参数
+
+# 参数
 ApiToken="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 Subdomain="subdomain"
 Domain="example.com"
@@ -7,12 +8,12 @@ DnsType="AAAA"
 TtlTime="60"
 Proxy="false"
 CloudflareApi="https://api.cloudflare.com/client/v4"
-#读取本地IP
+# 读取本地IP
 LocalIp=$(ip -6 addr | grep :: | awk -F '[ \t]+|/' '{print $3}' | grep -v ^::1 | grep -v ^fe80)
-#读取Zone ID
+# 读取Zone ID
 GetZoneApi="${CloudflareApi}/zones"
 GetZone=$(curl -s -X GET "$GetZoneApi" -H "Authorization: Bearer $ApiToken" -H "Content-Type:application/json")
-#读取反馈状态
+# 读取反馈状态
 GetZoneSuccess=$(echo "$GetZone" | jq -r ".success")
 if [[ $GetZoneSuccess = "true" ]]; then
     echo "GetZoneOK"
@@ -21,10 +22,10 @@ else
     exit 0
 fi
 ZoneId=$(echo "$GetZone" | jq -r ".result[0].id")
-#读取Record ID & IP
+# 读取Record ID & IP
 GetRecordApi="${CloudflareApi}/zones/${ZoneId}/dns_records?type=${DnsType}&name=${Subdomain}.${Domain}"
 GetRecord=$(curl -s -X GET "$GetRecordApi" -H "Authorization: Bearer $ApiToken" -H "Content-Type:application/json")
-#读取反馈状态
+# 读取反馈状态
 GetRecordSuccess=$(echo "$GetRecord" | jq -r ".success")
 if [[ $GetRecordSuccess = "true" ]]; then
     echo "GetRecordOK"
@@ -32,30 +33,30 @@ else
     echo "GetRecordNG"
     exit 0
 fi
-#提取在线记录的ID与IP
+# 提取在线记录的ID与IP
 RecordId=$(echo "$GetRecord" | jq -r ".result[0].id")
 RecordIp=$(echo "$GetRecord" | jq -r ".result[0].content")
-#判断IP是否相同
+# 判断IP是否相同
 if [[ $RecordIp = $LocalIp ]]; then
     echo "IpNoChange"
     exit 0
 fi
-#如果ID不存在建立新记录
+# 如果ID不存在建立新记录
 if [[ $RecordId = "null" ]]; then
     CreateRecordApi="$CloudflareApi/zones/${ZoneId}/dns_records"
     CreateRecord=$(curl -s -X POST "$CreateRecordApi" -H "Authorization: Bearer $ApiToken" -H "Content-Type:application/json" --data "{\"type\":\"$DnsType\",\"name\":\"$Subdomain.$Domain\",\"content\":\"$LocalIp\",\"ttl\":$TtlTime,\"proxied\":$Proxy}")
-    #读取反馈状态
+    # 读取反馈状态
     CreateRecordSuccess=$(echo "$CreateRecord" | jq -r ".success")
     if [[ $CreateRecordSuccess = "true" ]]; then
         echo "CreateRecordOK"
     else
         echo "CreateRecordNG"
     fi
-#更新记录
+# 更新记录
 else
     UpdateRecordApi="$CloudflareApi/zones/${ZoneId}/dns_records/${RecordId}"
     UpdateRecord=$(curl -s -X PUT "$UpdateRecordApi" -H "Authorization: Bearer $ApiToken" -H "Content-Type:application/json" --data "{\"type\":\"$DnsType\",\"name\":\"$Subdomain.$Domain\",\"content\":\"$LocalIp\",\"ttl\":$TtlTime,\"proxied\":$Proxy}")
-    #读取反馈状态
+    # 读取反馈状态
     UpdateRecordSuccess=$(echo "$UpdateRecord" | jq -r ".success")
     if [[ $UpdateRecordSuccess = "true" ]]; then
         echo "UpdateRecordOK"
