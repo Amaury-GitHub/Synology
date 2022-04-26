96dcf85af3e317c237473f6b8292d6f4767d0829
Amaury <amaury.data@gmail.com>
2022-04-26 21:03:22 +0800

    Update Clash.sh

--- Clash/Clash.sh
+++ Clash/Clash.sh
@@ -27,8 +27,8 @@ iptables -t mangle -A CLASH -d 224.0.0.0/4 -j RETURN
 iptables -t mangle -A CLASH -d 240.0.0.0/4 -j RETURN
 # 跳过 NTP 端口
 iptables -t mangle -A CLASH -p udp --dport 123 -j RETURN
-# 所有流量转发到 TPROXY 端口，并设置 mark
+# 内网网段的流量转发到 TPROXY 端口，并设置 mark
 iptables -t mangle -A CLASH -j TPROXY -p tcp -s 192.168.1.0/24 --on-port 7893 --tproxy-mark 1
 iptables -t mangle -A CLASH -j TPROXY -p udp -s 192.168.1.0/24 --on-port 7893 --tproxy-mark 1
-# 最后让所有流量通过 CLASH 链进行处理
+# 所有流量通过 CLASH 链进行处理
 iptables -t mangle -A PREROUTING -j CLASH
