31d2b6d208c50057bb4ae8f4e8426a2d1c163da6
Amaury <amaury.data@gmail.com>
2022-04-26 12:47:18 +0800

    Update Clash.sh

--- Clash/Clash.sh
+++ Clash/Clash.sh
@@ -2,6 +2,12 @@
 
 # 等待网卡就绪
 sleep 1m
+# 开启IP转发
+sysctl -w net.ipv4.ip_forward=1
+iptables -t nat -A POSTROUTING -o ppp0 -j MASQUERADE
+# 修复 ICMP
+sysctl -w net.ipv4.conf.all.route_localnet=1
+iptables -t nat -A PREROUTING -p icmp -d 198.18.0.0/16 -j DNAT --to-destination 127.0.0.1
 # 添加路由规则
 ip rule add fwmark 1 lookup 100
 ip route add local default dev lo table 100
@@ -16,8 +22,10 @@ iptables -t mangle -A CLASH -d 172.16.0.0/12 -j RETURN
 iptables -t mangle -A CLASH -d 192.168.0.0/16 -j RETURN
 iptables -t mangle -A CLASH -d 224.0.0.0/4 -j RETURN
 iptables -t mangle -A CLASH -d 240.0.0.0/4 -j RETURN
-# 转发到 tproxy-port 端口，并设置 mark
-iptables -t mangle -A CLASH -j TPROXY -p tcp --on-port 7893 --tproxy-mark 1
-iptables -t mangle -A CLASH -j TPROXY -p udp --on-port 7893 --tproxy-mark 1
+# 内网IP的所有流量转发到 TPROXY 端口，并设置 mark
+iptables -t mangle -A CLASH -j TPROXY -p tcp -s 192.168.1.0/24 --on-port 7893 --tproxy-mark 1
+iptables -t mangle -A CLASH -j TPROXY -p udp -s 192.168.1.0/24 --on-port 7893 --tproxy-mark 1
+# 转发所有 DNS 查询到 1053 端口
+iptables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to 1053
 # 最后让所有流量通过 CLASH 链进行处理
 iptables -t mangle -A PREROUTING -j CLASH
